4c2df9ea3e1dc068b6630f5bab7ffeb32c07a1f8
sumdmt <sumeyyedemet@gmail.com>
2024-05-30 16:54:22 +0300

    fix

--- calculator.js
+++ calculator.js
@@ -1,20 +1,30 @@
-(function () {
-  let screen = document.querySelector(".inner");
-  let buttons = document.querySelectorAll("btn");
-  let clear = document.querySelector(".clear");
-  let equal = document.querySelector(".equal");
+let screen = document.querySelector(".inner");
+let buttons = document.querySelectorAll(".btn");
+let clear = document.querySelector(".clear");
+let equal = document.querySelector(".equal");
+
+screen.value = "0";
+
+grabAllButtons();
+
+equal.addEventListener("click", showResult);
+
+clear.addEventListener("click", clearScreen);
+
+function clearScreen() {
+  // prettier-ignore
+  return screen.value = "0";
+}
+
+function showResult() {
+  return screen.value == "" ? null : (screen.value = eval(screen.value));
+}
+
+function grabAllButtons() {
   buttons.forEach(function (button) {
     button.addEventListener("click", function (e) {
       let value = e.target.dataset.num;
       screen.value += value;
     });
   });
-  equal.addEventListener("click", function (e) {
-    if (screen.value === "") {
-    } else {
-      let answer = eval(screen.value);
-      screen.value = answer;
-    }
-    clear.addEventListener("click", function (e) {});
-  });
-})();
+}
